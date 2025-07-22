d667aed2caa51fc0ca35722f585504fb58199774
eibenp <peter@eiben.hu>
2025-07-22 19:42:37 +0200

    Define buzzer command

--- src/commands.ts
+++ src/commands.ts
@@ -149,6 +149,13 @@ const commands = {
       on: 1,
     },
   },
+  buzzer: { // this is not persistent, AC does not remember it, but value = 1 can be used to send a command without a confirmation beep
+    code: 'Buzzer_ON_OFF',
+    value: {
+      off: 1,
+      on: 0,
+    },
+  },
 };
 
 export default commands;
